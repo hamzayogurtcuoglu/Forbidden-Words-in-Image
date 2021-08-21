618e359e030e0fccd10f967eb31dffe30964bb1a
hamzayogurtcuoglu <hamzayogurtcuoglu@gmail.com>
2021-08-21 19:01:00 +0300

    The script that finds the forbidden word in the given image.

---
+++ ImageDetect.py
@@ -0,0 +1,42 @@
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", required=True,help="Image Path")
+args = vars(ap.parse_args())
+
+imgPath = os.path.join(os.getcwd(), args['path']);
+img = cv2.imread(imgPath)
+
+backListWord = open('Blacklistwords.txt', 'r')
+lines = backListWord.readlines()
+lines = list(map(lambda x: x.replace('\n', ''), lines))
+
+try:
+    lines.remove('')
+    lines.remove(' ')
+except:
+    print("")
+
+forbiddenWordDetected = False
+
+for i in range(1,7):
+    try:
+        custom_config = r'--oem 3 -l tur --psm '
+        custom_config += str(i)
+        text = pytesseract.image_to_string(img, config=custom_config)
+        print(text)
+        print("-------------------------------")
+        for l in lines:
+            if text.lower().find(l.lower()) != -1 :
+                forbiddenWordDetected = True
+    except:
+        continue
+
+if forbiddenWordDetected:
+    print ("Danger detected.")
+else:
+    print("The danger could not be detected.")
---
+++ package.sh
@@ -0,0 +1,7 @@
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+sudo apt install python3-pip
+pip install pytesseract
+pip install opencv-python
+pip install matplotlib
+sudo apt-get install tesseract-ocr-tur
